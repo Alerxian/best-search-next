5c9c61f9029f4dde024058cd520de99a5b7cd2d6
unknown <1967376524@qq.com>
2023-02-04 16:21:40 +0800

    build: build project

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { NextPageWithLayout } from './page';
+
+interface AppPropsWithLayout extends AppProps {
+  Component: NextPageWithLayout;
+}
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout || ((page) => page);
+  return getLayout(<Component {...pageProps} />);
+}
